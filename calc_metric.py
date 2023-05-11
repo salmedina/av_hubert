467ee0d9d0a4afbaef2fe9b422cafb98a88a3c6f
salmedina <salvadom@andrew.cmu.edu>
2023-05-11 00:33:28 -0400

    Fix calc_metrics to get correct error rate according to metric name

--- calc_metric.py
+++ calc_metric.py
@@ -24,19 +24,27 @@ def parse_args():
                         help='Epoch number to be processed')
     parser.add_argument('-r', '--run_name', type=str, default='bright-rain-92',
                         help='Name of the run to be processed')
+    parser.add_argument('-m', '--metric', type=str, default='wer',
+                        help='Metric to be computed: cer, wer')
     return parser.parse_args()
+
+
+def get_metric_func(metric_label):
+    if metric_label == 'cer':
+        return calc_cer
+    elif metric_label == 'wer':
+        return calc_wer
+    
+    return None
 
 
 def main(args):
     epoch = args.epoch
     run_name = args.run_name
-    metric = args.metric
+    metric_name = args.metric
 
-    assert metric in ['cer', 'wer'], f'Metric has to be wer or cer'
-    if metric == 'wer':
-        calc_metric_func = calc_wer
-    elif metric == 'cer'
-        calc_metric_func = calc_cer
+    assert metric_name in ['cer', 'wer'], f'Metric has to be wer or cer'
+    calc_metric_func = get_metric_func(metric_name)
 
     transcripts = edict(imt=edict(path='/mnt/local/salmedina/Data/Processed/index/transcripts.csv',
                                   type='imt'),
@@ -47,14 +55,14 @@ def main(args):
                                type=transcripts.lrs3.type)
 
     vsr_dir = Path(f'/mnt/local/salmedina/Output/VSR/lrs3_test/vsr/{run_name}-{epoch}/tsv')
-    output_dir = Path(f'/mnt/local/salmedina/Output/VSR/lrs3_test/vsr/{run_name}-{epoch}/{metric}')
+    output_dir = Path(f'/mnt/local/salmedina/Output/VSR/lrs3_test/vsr/{run_name}-{epoch}/{metric_name}')
 
     output_dir.mkdir(parents=True, exist_ok=True)
 
     for tsv_path in vsr_dir.glob('*.tsv'):
         output_path = output_dir / tsv_path.name
         lines = list()
-        lines.append('\t'.join(['score', metric, 'hits', 'sub', 'ins', 'del', 'hyp', 'ref']))
+        lines.append('\t'.join(['score', metric_name, 'hits', 'sub', 'ins', 'del', 'hyp', 'ref']))
 
         for pred in [l.strip().split('\t') for l in open(tsv_path).readlines()]:    
             sid = tsv_path.stem
@@ -62,9 +70,13 @@ def main(args):
             hyp = normalize_text(pred[1])
 
             metrics = calc_metric_func(ref, hyp)
+            if metric_name == 'cer':
+                error_rate = metrics.cer
+            elif metric_name == 'wer':
+                error_rate = metrics.wer
 
             lines.append('\t'.join([pred[0],
-                                    f'{metrics.cer:.05f}',
+                                    f'{error_rate:.05f}',
                                     f'{metrics.hits}',
                                     f'{metrics.substitutions}',
                                     f'{metrics.insertions}',
